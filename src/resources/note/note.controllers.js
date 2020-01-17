0ab796f7a18ac978744541198672b00ad23805b6
Confidence Okoghenun <confidenceboi@gmail.com>
2020-01-17 10:26:05 +0100

    fix: Fixing time zone diff 7

--- src/resources/note/note.controllers.js
+++ src/resources/note/note.controllers.js
@@ -64,8 +64,8 @@ export default {
       // const now = moment().format('YYYY-MM-DDTHH:mm:ss')
       // const date = moment(updatedDoc.due).format('YYYY-MM-DDTHH:mm:ss')
 
-      const now = moment(clientNow).toISOString()
-      const date = moment(updatedDoc.due).toISOString()
+      const now = moment(clientNow).format()
+      const date = moment(updatedDoc.due).format()
 
       // const now = moment.utc().local().toISOString()
       // const date = moment(updatedDoc.due).format()
