9ec3a9f0f4b13ab2a7b81e552cdc83ffa3ca8404
Confidence Okoghenun <confidenceboi@gmail.com>
2020-01-25 00:11:48 +0100

    fix: Fixxed spacing issue on notification and removed time

--- src/resources/note/note.controllers.js
+++ src/resources/note/note.controllers.js
@@ -18,9 +18,8 @@ agenda.define('send reminders', async job => {
   console.log('## fired reminder for ' + note._id)
 
   const payload = JSON.stringify({
-    title: note.title.replace(/<\/?[^>]+(>|$)/g, ""),
-    body: note.content.replace(/<\/?[^>]+(>|$)/g, ""),
-    time: moment(note.due).format('MMMM Do YYYY, h:mm a')
+    title: note.title.replace(/<\/?[^>]+(>|$)/g, ' ').replace(/  +/g, ' '),
+    body: note.content.replace(/<\/?[^>]+(>|$)/g, ' ').replace(/  +/g, ' ')
   })
 
   webpush
